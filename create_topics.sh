ce927353c56d03cdc0ee33688c05092c86c08b79
markteehan_Ubuntu <teehan@gmail.com>
2018-06-27 23:16:58 +0800

    gdeltevent6

--- create_topics.sh
+++ create_topics.sh
@@ -1,3 +1,3 @@
 cd $CONFLUENT_HOME
 
-bin/kafka-topics --create --zookeeper localhost:2181 --topic gdeltEvents --partitions 8 --replication-factor 1
+bin/kafka-topics --create --zookeeper localhost:2181 --topic gdeltEvent6 --partitions 8 --replication-factor 1
